131c4f637e4afc5af7d55b628ea1f3f7329c37b5
joyahmed <joy_ahmed_007@yahoo.com>
2022-05-18 21:41:34 +0600

    buttons added

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class App(object):
 
         # Bottom Frame
 
-        self.bottom = Frame(root, height=500, bg='#adff2f')
+        self.bottom = Frame(root, height=500, bg='#00FFFF')
         self.bottom.pack(fill=X)
 
         # Heading, image, and date
@@ -27,11 +27,29 @@ class App(object):
             self.top, image=self.top_image, bg='white')
         self.top_image_label.place(x=120, y=10)
         self.heading = Label(
-            self.top, text='Book Store Management App', font='roboto 15 bold', fg='#ffa500', bg='white')
+            self.top, text='Book Store Management App', font='roboto 15 bold', fg='#32CD32', bg='white')
         self.heading.place(x=260, y=60)
         self.date_label = Label(
-            self.top, text='Date: ' + str(date), font='roboto 12 bold', bg='white', fg='#ffa500')
+            self.top, text='Date: ' + str(date), font='roboto 12 bold', bg='white', fg='#32CD32')
         self.date_label.place(x=490, y=10)
+
+        # Add Button
+
+        self.add_button_icon = PhotoImage(file='icons/add_book.png')
+        self.add_button = Button(
+            self.bottom, text='        Add Books           ', font='roboto 12 bold')
+        self.add_button.config(
+            image=self.add_button_icon, compound=LEFT)
+        self.add_button.place(x=220, y=100)
+
+        # Book List Button
+
+        self.list_button_icon = PhotoImage(file='icons/booklist.png')
+        self.list_button = Button(
+            self.bottom, text='     Manage Books      ', font='roboto 12 bold', pady=8)
+        self.list_button.config(
+            image=self.list_button_icon, compound=LEFT)
+        self.list_button.place(x=220, y=200)
 
 
 def main():
